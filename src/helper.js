7e82d8c2932e7a6d0dd2f4e8f4786a77aa8ee7d6
Oscar Bryant <oscarcbryant@gmail.com>
2022-02-22 08:10:48 +1100

    cleaning up styling in html

--- src/helper.js
+++ src/helper.js
@@ -91,24 +91,25 @@ let manager;
     return internString;
     }
 
-    htmlStarter = `!DOCTYPE html>
-<html lang="en">
-<head>
+    htmlStarter = 
+    
+    `<html lang="en">
+        <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<link rel="stylesheet" href="style.css">
-<body>
-    <div id="heading">
-    <header id ="heading-text">My team</header>
-    </div>
-    <main> <${generateManagerCard(manager)} + ${generateEngineerCard(allEngineers)} + ${generateInternCard(allInterns)}>
-    </main>
-    </body>
-    </html>
-    `
+        </head>
+            <link rel="stylesheet" href="style.css">
+            <body>
+        <div id="heading">
+            <header id ="heading-text">My team</header>
+        </div>
+    <main> ${generateManagerCard(manager)}  ${generateEngineerCard(allEngineers)}  ${generateInternCard(allInterns)}
+            </main>
+            </body>
+            </html>`
+
     return htmlStarter;
 }
 
